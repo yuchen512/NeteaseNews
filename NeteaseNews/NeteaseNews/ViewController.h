c85750aa77c4dcb70394ca5a7e26fda92a9d0d6c
yuchen512 <410324575@qq.com>
2015-06-29 17:33:26 +0800

    初始化项目

---
+++ NeteaseNews/NeteaseNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NeteaseNews
+//
+//  Created by Yu chen on 15/6/29.
+//  Copyright (c) 2015年 Yu chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NeteaseNews/NeteaseNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NeteaseNews
+//
+//  Created by Yu chen on 15/6/29.
+//  Copyright (c) 2015年 Yu chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
